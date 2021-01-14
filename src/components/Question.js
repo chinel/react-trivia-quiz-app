3c40277d392d113e858c29d108aeab8e0db6ed71
nelo <nelokwaz@gmail.com>
2021-01-14 14:02:26 +0100

    made some corrections

--- src/components/Question.js
+++ src/components/Question.js
@@ -3,7 +3,7 @@ import React from "react";
 const sampleAnswers = ["One", "Two", "Three", "Four"];
 
 export default function Question({ question }) {
-  const answers = [...question.incorrect_answers, question.incorrect_answer];
+  const answers = [...question.incorrect_answers, question.correct_answer];
 
   return (
     <div className="question">
