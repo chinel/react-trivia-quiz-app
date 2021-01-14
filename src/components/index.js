8afbb5a6a841f458e38831bfeda0a1c82fe8dc9f
nelo <nelokwaz@gmail.com>
2021-01-14 12:31:02 +0100

    added the structure of the app component

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Question } from "./Question";
+export { default as CategorySelector } from "./CategorySelector";
+export { default as ResultModal } from "./ResultModal";
+export { default as Scoreboard } from "./Scoreboard";
