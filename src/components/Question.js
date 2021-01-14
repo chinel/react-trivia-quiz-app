eb2e2cc0799b342933bc1eac97e95e9fe46ff04f
nelo <nelokwaz@gmail.com>
2021-01-14 13:53:49 +0100

    combined all answers

--- src/components/Question.js
+++ src/components/Question.js
@@ -3,6 +3,8 @@ import React from "react";
 const sampleAnswers = ["One", "Two", "Three", "Four"];
 
 export default function Question({ question }) {
+  const answers = [...question.incorrect_answers, question.incorrect_answer];
+
   return (
     <div className="question">
       <h2>Question Here</h2>
