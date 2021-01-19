639185e3f0e4b5ed378e8cdcde7d3432b60e531e
nelo <nelokwaz@gmail.com>
2021-01-18 17:19:07 -1200

    unset question if there is an error retrieving questions

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
-import { CategorySelector, Loading, Question, ResultModal, Scoreboard } from "./components";
+import {
+  CategorySelector,
+  Loading,
+  Question,
+  ResultModal,
+  Scoreboard,
+} from "./components";
 
 export default function App() {
   const [question, setQuestion] = useState(null);
@@ -8,14 +14,20 @@ export default function App() {
   const [isCorrect, setIsCorrect] = useState(null);
   const getQuestions = React.useCallback(() => {
     setIsCorrect(null);
+
     let url = "https://opentdb.com/api.php?amount=1";
 
     if (selectedCategory !== "any") url += `&category=${selectedCategory}`;
 
     fetch(url)
       .then((res) => res.json())
-      .then((data) => setQuestion(data.results[0]))
-      .catch((err) => console.error(err));
+      .then((data) => {
+        setQuestion(data.results[0]);
+      })
+      .catch((err) => {
+        setQuestion(null);
+        console.error(err);
+      });
   }, [selectedCategory]);
 
   useEffect(() => {
@@ -25,12 +37,18 @@ export default function App() {
   function handleQuestionAnswered(answer) {
     const isAnswerCorrect = answer === question.correct_answer;
     setIsCorrect(isAnswerCorrect);
-  } 
+  }
 
   return (
     <div className="app">
       {/* show the result modal ----------------------- */}
-      {isCorrect !== null &&  <ResultModal isCorrect={isCorrect} question={question} getQuestions={getQuestions}/> 
+      {isCorrect !== null && (
+        <ResultModal
+          isCorrect={isCorrect}
+          question={question}
+          getQuestions={getQuestions}
+        />
+      )}
 
       {/* question header ----------------------- */}
       <div className="question-header">
